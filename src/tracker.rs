7d314c60b191e7b5e3038212bb9b712c157217fd
p4xxus <79471967+p4xxus@users.noreply.github.com>
2025-11-02 20:51:37 +0100

    added IPv6 support as specified in BEP07

--- src/tracker.rs
+++ src/tracker.rs
@@ -1,9 +1,9 @@
-use std::net::SocketAddr;
-
+use crate::{
+    torrent::InfoHash,
+    tracker::{peers::PeerConnections, peers6::Peer6Connections},
+};
 use serde::{Deserialize, Serialize};
-use thiserror::Error;
-
-use crate::{torrent::InfoHash, tracker::peers::PeerConnections};
+use std::net::SocketAddr;
 
 #[derive(Debug, Clone, Serialize)]
 pub struct TrackerRequest<'a> {
@@ -105,15 +105,13 @@ pub struct TrackerResponse {
     /// Each peer is represented using 6 bytes.
     /// The first 4 bytes are the peer's IP address and the last 2 bytes are the peer's port number.
     peers: PeerConnections,
+    peers6: Option<Peer6Connections>,
 }
 impl TrackerResponse {
     pub fn get_peers(&self) -> Vec<SocketAddr> {
-        self.peers
-            .0
-            .clone()
-            .into_iter()
-            .map(SocketAddr::from)
-            .collect()
+        let mut all_peers = self.peers.0.clone();
+        all_peers.extend_from_slice(&self.peers6.clone().unwrap_or_default().0);
+        all_peers.into_iter().map(SocketAddr::from).collect()
     }
 }
 
@@ -129,9 +127,10 @@ mod peers {
         de::{self, Visitor},
     };
 
-    #[derive(Debug, Clone)]
-    pub struct PeerConnections(pub Vec<SocketAddr>);
     struct PeersVisitor;
+
+    #[derive(Debug, Clone)]
+    pub(super) struct PeerConnections(pub(super) Vec<SocketAddr>);
 
     impl<'de> Visitor<'de> for PeersVisitor {
         type Value = PeerConnections;
@@ -144,24 +143,26 @@ mod peers {
         where
             E: de::Error,
         {
-            if v.len() % 6 != 0 {
-                return Err(de::Error::custom(format!(
+            let length = v.len();
+            if length % 6 == 0 {
+                Ok(PeerConnections(
+                    v.chunks_exact(6)
+                        .map(|chunk| {
+                            let &[a, b, c, d, p1, p2] =
+                                chunk.try_into().expect("the chunk is of length 6");
+                            SocketAddr::new(
+                                Ipv4Addr::new(a, b, c, d).into(),
+                                u16::from_be_bytes([p1, p2]),
+                            )
+                        })
+                        .collect(),
+                ))
+            } else {
+                Err(de::Error::custom(format!(
                     "Bytes which length is a multiple of 6. Got {:?}",
                     v.len()
-                )));
+                )))
             }
-            Ok(PeerConnections(
-                v.chunks_exact(6)
-                    .map(|chunk| {
-                        let &[a, b, c, d, p1, p2] =
-                            chunk.try_into().expect("the chunk is of length 6");
-                        SocketAddr::new(
-                            Ipv4Addr::new(a, b, c, d).into(),
-                            u16::from_be_bytes([p1, p2]),
-                        )
-                    })
-                    .collect(),
-            ))
         }
     }
 
@@ -209,17 +210,93 @@ mod peers {
     }
 }
 
+mod peers6 {
+    use std::{
+        fmt,
+        net::{IpAddr, Ipv6Addr, SocketAddr},
+    };
+
+    use serde::{
+        Deserialize, Deserializer,
+        de::{self, Visitor},
+    };
+
+    struct Peers6Visitor;
+
+    #[derive(Debug, Clone)]
+    pub(super) struct Peer6Connections(pub(super) Vec<SocketAddr>);
+
+    impl Default for Peer6Connections {
+        fn default() -> Self {
+            Self(Default::default())
+        }
+    }
+
+    impl<'de> Visitor<'de> for Peers6Visitor {
+        type Value = Peer6Connections;
+
+        fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+            formatter.write_str("A string of multiples of 18 bytes")
+        }
+
+        fn visit_bytes<E>(self, v: &[u8]) -> Result<Self::Value, E>
+        where
+            E: de::Error,
+        {
+            let length = v.len();
+            if length % 18 == 0 {
+                Ok(Peer6Connections(
+                    v.chunks_exact(18)
+                        .map(|chunk| {
+                            let ipv6_addr = Ipv6Addr::from_bits(u128::from_be_bytes(
+                                chunk[..16].try_into().expect("chunk has 18 bytes"),
+                            ));
+                            let ip_addr = IpAddr::V6(ipv6_addr);
+                            let port = u16::from_be_bytes(
+                                chunk[16..18].try_into().expect("chunk has 18 bytes"),
+                            );
+
+                            SocketAddr::new(ip_addr, port)
+                        })
+                        .collect(),
+                ))
+            } else {
+                Err(de::Error::custom(format!(
+                    "Bytes which length is a multiple of 18. Got {:?}",
+                    v.len()
+                )))
+            }
+        }
+    }
+
+    impl<'de> Deserialize<'de> for Peer6Connections {
+        fn deserialize<D>(deserializer: D) -> Result<Peer6Connections, D::Error>
+        where
+            D: Deserializer<'de>,
+        {
+            deserializer.deserialize_bytes(Peers6Visitor)
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
     use serde_bencode;
     #[test]
     fn test_deserialize_compact_peers() {
-        // A dictionary with a 'peers' key containing a 6-byte string
-        let compact_peers = b"d8:intervali0e5:peers6:\x01\x02\x03\x04\x1a\x0ee";
-        let expected_socket_addr = "1.2.3.4:6670".parse::<SocketAddr>().unwrap();
-        let tracker_response: TrackerResponse = serde_bencode::from_bytes(compact_peers).unwrap();
-        assert_eq!(tracker_response.get_peers(), vec![expected_socket_addr]);
+        // A dictionary with both 'peers' (IPv4) and 'peers6' (IPv6) keys
+        let bencoded_response_with_ipv6 = b"d8:intervali0e5:peers6:\x7f\x00\x00\x01\x1a\x0e6:peers618:\x20\x01\x0d\xb8\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x41\x37\x01\xbbee";
+        let expected_socket_addrs_with_ipv6 = vec![
+            "127.0.0.1:6670".parse::<SocketAddr>().unwrap(),
+            "[2001:db8::4137]:443".parse::<SocketAddr>().unwrap(),
+        ];
+        let tracker_response_with_ipv6: TrackerResponse =
+            serde_bencode::from_bytes(bencoded_response_with_ipv6).unwrap();
+        assert_eq!(
+            tracker_response_with_ipv6.get_peers(),
+            expected_socket_addrs_with_ipv6
+        );
     }
 
     #[test]
