3796aaaba9dc3e97e989902e4640472068af69d3
p4xxus <79471967+p4xxus@users.noreply.github.com>
2025-11-06 16:59:31 +0100

    we now have a faster algorithm for the tracker request again

--- src/peer_manager/peer_fetcher.rs
+++ src/peer_manager/peer_fetcher.rs
@@ -1,5 +1,7 @@
-use std::{net::SocketAddr, time::Duration};
+use futures_util::FutureExt;
+use std::{collections::HashSet, net::SocketAddr, time::Duration};
 use tokio::sync::mpsc;
+use tokio_stream::StreamExt;
 
 use crate::{
     BLOCK_MAX, Peer, PeerManager, TrackerRequest,
@@ -9,26 +11,29 @@ use crate::{
     tracker::TrackerResponse,
 };
 
-const DEFAULT_TRACKER_TIMEOUT: Duration = Duration::from_secs(120);
+const DEFAULT_TRACKER_INTERVAL: Duration = Duration::from_secs(120);
+/// specifies after how long we should stop the request to the tracker (ideally before tcp connection timeout occurs)
+const TRACKER_REQUEST_TIMEOUT: Duration = Duration::from_secs(10);
+const NUM_ENOUGH_PEERS_TO_START: usize = 5;
 
 #[derive(Debug)]
 pub(super) struct PeerFetcher {
     pub(super) tx: mpsc::Sender<ReqMsgFromPeer>,
     /// something like [ [ tracker1, tracker2 ], [ backup1 ] ]
     pub(super) announce_list: AnnounceList,
-    tracker_timeout: Duration,
+    tracker_req_interval: Duration,
 }
 
 impl PeerFetcher {
     pub(in crate::peer_manager) fn get_tracker_req_interval(&self) -> Duration {
-        self.tracker_timeout
+        self.tracker_req_interval
     }
 
     pub(super) fn new(tx: mpsc::Sender<ReqMsgFromPeer>, announce_list: AnnounceList) -> Self {
         Self {
             tx,
             announce_list,
-            tracker_timeout: DEFAULT_TRACKER_TIMEOUT,
+            tracker_req_interval: DEFAULT_TRACKER_INTERVAL,
         }
     }
 
@@ -54,24 +59,49 @@ impl PeerFetcher {
     pub(super) async fn get_tracker_response<'a>(
         &mut self,
         tracker_request: TrackerRequest<'a>,
-    ) -> Option<TrackerResponse> {
-        for tier in self.announce_list.0.iter_mut() {
-            if let Some((url_index_in_tier, tracker_response)) = tracker_request
-                .get_first_response_in_list(tier.clone())
-                .await
-            {
-                let url = tier.remove(url_index_in_tier);
-                tier.insert(0, url);
+    ) -> Option<(TrackerResponse, Vec<SocketAddr>)> {
+        // construct the stream which gets the first response of each tier with a timeout
+        // essentially mutably returns the peer, the index of the url in the peer and the response
+        let tracker_responses = futures_util::stream::FuturesUnordered::from_iter(
+            self.announce_list
+                .0
+                .iter_mut()
+                .map(|tier| {
+                    tokio::time::timeout(
+                        TRACKER_REQUEST_TIMEOUT,
+                        tracker_request.get_first_response_in_list(tier.clone()),
+                    )
+                    .map(move |res| (tier, res))
+                })
+                .into_iter(),
+        );
+        tokio::pin!(tracker_responses);
 
-                return Some(tracker_response);
+        let mut first_response = None;
+        let mut peer_list = HashSet::new();
+
+        while let Some((tier, Ok(Some((url_index_in_tier, tracker_response))))) =
+            dbg!(tracker_responses.next().await)
+        {
+            // extend peer list
+            peer_list.extend(tracker_response.get_peers());
+
+            // update tier ranking
+            first_response.get_or_insert(tracker_response);
+            let url = tier.remove(url_index_in_tier);
+            tier.insert(0, url);
+
+            // if we got enough peers, break
+            if peer_list.len() >= NUM_ENOUGH_PEERS_TO_START {
+                break;
             }
         }
 
-        None
+        first_response.map(|res| (res, peer_list.into_iter().collect()))
     }
 
     pub(super) fn set_tracker_req_interval(&mut self, timeout: usize) {
-        self.tracker_timeout = Duration::from_secs(timeout as u64);
+        self.tracker_req_interval = Duration::from_secs(timeout as u64);
     }
 }
 
@@ -83,13 +113,13 @@ impl PeerManager {
         let tracker_request =
             TrackerRequest::new(&info_hash, PEER_ID, client_options.port, left_to_download);
 
-        if let Some(res) = self
+        if let Some((res, peers)) = self
             .peer_fetcher
             .get_tracker_response(tracker_request)
             .await
         {
             self.peer_fetcher
-                .add_peers_to_manager(info_hash, res.get_peers())
+                .add_peers_to_manager(info_hash, peers)
                 .await;
             self.peer_fetcher.set_tracker_req_interval(res.interval);
         } else {
